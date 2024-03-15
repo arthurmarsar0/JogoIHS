7bc548b1598b90579ac7adbc4f6fd217314d9f66
DaniloLDCarvalho <110473965+DaniloLDCarvalho@users.noreply.github.com>
2024-03-15 14:10:03 -0300

    Update colors.py

--- colors.py
+++ colors.py
@@ -2,14 +2,16 @@ class Colors:
     dark_grey=(26,31,40)
     green=(47,230,23)
     red=(232,18,18)
-    orange=(226,116,17)
     yellow=(237,234,4)
-    purple=(166,0,247)
+    purple=(178,102,255)
     cyan=(21,204,209)
     blue=(13,64,216)
     dark_blue = (44,44,127)
     white = (255, 255, 255)
     light_blue = (59, 85, 162)
+    pink =(153,0,153)
+    light_purple =(51,0,102)
+    orange=(255,128,0)
 
     @classmethod
     def get_cell_colors(cls):
